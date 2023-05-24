7ab99a00c61c361ea9608b37eec1a88028ed8c1e
hou27 <ataj125@gmail.com>
2023-05-25 00:02:22 +0900

    [Remove] #1 불필요한 코드 제거

--- app.py
+++ app.py
@@ -8,14 +8,11 @@ app = Flask(__name__)
 @app.get("/compare_images")
 def compare_images():
     try:
+        # 파일 경로를 query string에서 가져오기
         file1_path = request.args.get("file1")
         file2_path = request.args.get("file2")
-        # # 파일 경로를 request body에서 가져오기
-        # data = request.get_json()
-        # file1_path = data["file1"]
-        # file2_path = data["file2"]
 
-        # TODO: 파일 경로를 이용하여 이미지 비교 작업 수행
+        # 이미지 유사도 계산
         similarity = match(file1_path, file2_path)
 
         # 결과를 JSON 형식으로 반환
