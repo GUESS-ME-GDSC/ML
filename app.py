96a36bde7450e4f0ef3be48e054dbff0abf2c97d
hou27 <ataj125@gmail.com>
2023-05-24 19:44:10 +0900

    [Feat] #1 gui 작업 코드 제거 및 compare_images function 개선

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
+
+from structural_similarity import match
 
 app = Flask(__name__)
 
 
-@app.route("/compare_images", methods=["POST"])
+@app.post("/compare_images")
 def compare_images():
-    file1 = request.files["file1"]
-    file2 = request.files["file2"]
+    try:
+        # 파일 경로를 request body에서 가져오기
+        data = request.get_json()
+        file1_path = data["file1"]
+        file2_path = data["file2"]
 
-    # 이미지 처리 및 비교 작업 수행
-    filename1 = file1.filename
-    filename2 = file2.filename
+        # TODO: 파일 경로를 이용하여 이미지 비교 작업 수행
+        similarity = match(file1_path, file2_path)
 
-    return f"Image 1: {filename1}, Image 2: {filename2}"
+        # 결과를 JSON 형식으로 반환
+        response = {"similarity": similarity}
+        return jsonify(response), 200
+
+    except Exception as e:
+        # 오류 발생 시 오류 메시지와 함께 오류 상태 코드 반환
+        response = {"error": str(e)}
+        return jsonify(response), 400
 
 
 if __name__ == "__main__":
-    app.run()
+    print("Hi")
+    app.run(debug=True, port=4000)
--- structural_similarity.py
+++ structural_similarity.py
@@ -2,6 +2,7 @@ import cv2
 from skimage.metrics import structural_similarity as ssim
 import matplotlib.pyplot as plt
 
+
 # 컨투어 검출 함수
 def detect_contours(image):
     # 이미지를 그레이스케일로 변환
@@ -14,27 +15,30 @@ def detect_contours(image):
     cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
     return image
 
-def imshow(imgs):
-    # 이미지 출력
-    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
-    for i in range(len(imgs)):
-        image = imgs[i]
-        # 이미지 출력
-        axes[i].imshow(image)
-        axes[i].axis("off")
+# def imshow(imgs):
+#     # 이미지 출력
+#     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
-    plt.subplots_adjust(wspace=0.01)
+#     for i in range(len(imgs)):
+#         image = imgs[i]
+#         # 이미지 출력
+#         axes[i].imshow(image)
+#         axes[i].axis("off")
 
-    # 출력된 이미지 보이기
-    plt.show()
+#     plt.subplots_adjust(wspace=0.01)
+
+#     # 출력된 이미지 보이기
+#     plt.show()
+
 
 def match(path1, path2):
+    print(path1, path2)
     # read the images
     img1 = cv2.imread(path1)
     img2 = cv2.imread(path2)
     # display both images
-    imshow([img1, img2])
+    # imshow([img1, img2])
     # turn images to grayscale
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
@@ -43,6 +47,6 @@ def match(path1, path2):
     img2 = cv2.resize(img2, (300, 300))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    similarity_value = "{:.2f}".format(ssim(img1, img2)*100)
+    similarity_value = "{:.2f}".format(ssim(img1, img2) * 100)
 
     return float(similarity_value)
