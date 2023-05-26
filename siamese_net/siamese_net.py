d3887948854dbf9288622f7a448b37658cb5fe7f
hou27 <ataj125@gmail.com>
2023-05-26 23:11:42 +0900

    [Feat] #3 online image를 받아 처리하도록 수정

--- siamese_net/siamese_net.py
+++ siamese_net/siamese_net.py
@@ -1,36 +1,43 @@
-from siamese_net.siameseNetwork import SiameseNetwork
-from siamese_net.siameseNetworkDataset import SiameseNetworkDataset
-import torchvision.datasets as dset
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
 import torch
-from torch.autograd import Variable
 import torch.nn.functional as F
+from url_to_image import url_to_image
+from siamese_net.siameseNetwork import SiameseNetwork
 
 # Load trained model
 model = SiameseNetwork().cuda()
 model.load_state_dict(torch.load("siamese_model.pt"))
 
-siamese_dataset = SiameseNetworkDataset(
-    image1_path="/content/drive/MyDrive/siamese-net/content/sign_data/jeong/tmp2/dong1.jpeg",
-    image2_path="/content/drive/MyDrive/siamese-net/content/sign_data/jeong/tmp2/dong2.jpeg",
-    transform=transforms.Compose(
-        [transforms.Resize((100, 100)), transforms.ToTensor()]
-    ),
-    should_invert=False,
+# read the images online
+import cv2
+
+img1 = url_to_image(
+    "https://github.com/GUESS-ME-GDSC/Server/assets/65845941/64d3cf28-1665-448f-bfe8-96c1cfdc32fa"
+)
+img2 = url_to_image(
+    "https://github.com/GUESS-ME-GDSC/Server/assets/65845941/64d3cf28-1665-448f-bfe8-96c1cfdc32fa"
 )
 
+# turn images to grayscale
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-test_dataloader = DataLoader(siamese_dataset, num_workers=6, batch_size=1, shuffle=True)
-# dataiter = iter(test_dataloader)
-# x0,_,_ = next(dataiter)
+# resize images for comparison
+img1 = cv2.resize(img1, (100, 100))
+img2 = cv2.resize(img2, (100, 100))
 
-for i, data in enumerate(test_dataloader, 0):
-    # _,x1,label2 = next(dataiter)
-    x0, x1, label = data
-    concatenated = torch.cat((x0, x1), 0)
+img1_tensor = transforms.ToTensor()(img1)
+img2_tensor = transforms.ToTensor()(img2)
 
-    output1, output2 = model(Variable(x0).cuda(), Variable(x1).cuda())
-    euclidean_distance = F.pairwise_distance(output1, output2)
+# move the tensors to the desired device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+img1_tensor = img1_tensor.to(device)
+img2_tensor = img2_tensor.to(device)
 
-    print("Dissimilarity: {:.2f}".format(euclidean_distance.item()))
+img1_tensor = torch.unsqueeze(img1_tensor, 0)  # Add batch dimension to img1_tensor
+img2_tensor = torch.unsqueeze(img2_tensor, 0)
+
+output1, output2 = model(img1_tensor, img2_tensor)
+euclidean_distance = F.pairwise_distance(output1, output2)
+
+print("Dissimilarity: {:.2f}".format(euclidean_distance.item()))
