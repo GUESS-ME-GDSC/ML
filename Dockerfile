ed7defe59eb92804b23d90b4c0bf52285ed9ce3f
hou27 <ataj125@gmail.com>
2023-05-28 03:45:34 +0900

    [Feat] #3 cuda 없이 동작하도록 수정

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 FROM python:3.9-slim
 
 WORKDIR /app
-COPY requirements.txt .
+COPY requirements_siamese_net.txt .
 RUN pip install --upgrade pip \
   && apt-get update \
   && apt-get -y install libgl1-mesa-glx \
   && apt-get -y install libglib2.0-0 \
-  && pip install --no-cache-dir -r requirements.txt
+  && pip install --no-cache-dir -r requirements_siamese_net.txt
 COPY ./app.py .
-COPY ./structural_similarity.py .
+COPY ./siamese_net ./siamese_net
 
 # Run with Gunicorn
 CMD ["gunicorn", "--bind", "0.0.0.0:8000", "app:app"]
--- siamese_net/siamese_net.py
+++ siamese_net/siamese_net.py
@@ -8,7 +8,9 @@ from siamese_net.siameseNetwork import SiameseNetwork
 
 def checkSimilarity(path1, path2):
     # Load trained model
-    model = SiameseNetwork().cuda()
+    # model = SiameseNetwork().cuda()
+    # model.load_state_dict(torch.load("siamese_model.pt"))
+    model = SiameseNetwork()
     model.load_state_dict(torch.load("siamese_model.pt"))
 
     # read the images online
@@ -25,11 +27,6 @@ def checkSimilarity(path1, path2):
 
     img1_tensor = transforms.ToTensor()(img1)
     img2_tensor = transforms.ToTensor()(img2)
-
-    # move the tensors to the desired device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    img1_tensor = img1_tensor.to(device)
-    img2_tensor = img2_tensor.to(device)
 
     img1_tensor = torch.unsqueeze(img1_tensor, 0)  # Add batch dimension to img1_tensor
     img2_tensor = torch.unsqueeze(img2_tensor, 0)
